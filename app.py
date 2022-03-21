0476d8742414e1417d1c202c963c96680357ea47
yz695 <yz695@duke.edu>
2022-03-21 14:06:28 -0400

    update the loading and error catch

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import datetime
 import pandas as pd
 
 # Make a Dash app!
+from fintech_ibkr.synchronous_functions import fetch_contract_details
+
 app = dash.Dash(__name__)
 server=app.server
 
@@ -227,11 +229,20 @@ def update_candlestick_graph(n_clicks, currency_string, what_to_show,
     # First things first -- what currency pair history do you want to fetch?
     # Define it as a contract object!
     contract = Contract()
+    if currency_string.count(".") != 1:
+        return ("Error: wrong currency pairs format (" + currency_string + "), please check your input"), go.Figure()
     contract.symbol   = currency_string.split(".")[0]
     contract.secType  = 'CASH'
     contract.exchange = 'IDEALPRO' # 'IDEALPRO' is the currency exchange.
     contract.currency = currency_string.split(".")[1]
 
+    contract_detail = fetch_contract_details(contract)
+    if type(contract_detail) == str:
+        return ("Error: wrong currency pairs (" + currency_string + "), please check your input"), go.Figure()
+    else:
+        s = str(contract_detail).split(",")[10]
+        if s != currency_string:
+            return ("The system currency pairs " + s + " does not match your input " + currency_string), go.Figure()
     ############################################################################
     ############################################################################
     # This block is the one you'll need to work on. UN-comment the code in this
